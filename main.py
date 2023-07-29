ac82db4a1737aea8fba600438e0224a9682c58cf
Ahmad <mahmedraza90@gmail.com>
2023-07-29 22:34:00 +0500

    Final Commit
    The Project is ready

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ web_page = requests.get("https://web.archive.org/web/20200518073855/https://www.
 soup = BeautifulSoup(web_page,"html.parser")
 
 names = [item.getText() for item in soup.find_all(name = "h3",class_ = "title")]
-release_date = [int(item.getText()) for item in soup.find_all(name = "strong")]
 
-print(release_date)
+
+names.reverse()
+
+
+with open("Movies Names.txt","w",encoding="utf-8") as file:
+    for item in names:
+        file.write(item+"\n")
+
 print(names)
